8f97287bfe63f353f81447dc71fabda5caf232f1
Esphend <145009934+Esphend@users.noreply.github.com>
2024-09-02 11:03:04 +0500

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,3 +1,1 @@
-Python 2.7.11 (v2.7.11:6d1b6a68f775, Dec  5 2015, 20:40:30) [MSC v.1500 64 bit (AMD64)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> Print("Hello World!")
+Print("Hello World!") # Данная строка кода выводит на экран надпись "Hello World!" при помощи print'а
